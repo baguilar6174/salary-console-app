2e17ab2894c939698f0217de8883dfb4a04f19e5
Bryan Aguilar <bryan.aguilar6174@gmail.com>
2021-12-15 16:42:15 -0500

    Menu de opciones agregado
    * Validación de opción ingresada por el usuario
    * Obtener opción ingresada
    * Menú iterativo con acción de finalización
    
    Cambios en archivos:
    
    	modified:   src/app.js
    	new file:   src/helpers/handlerOutput.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+const { showMenu, pause } = require('./helpers/handlerOutput');
+
 // colores para la consola
 require('colors');
 
@@ -6,7 +8,48 @@ console.clear();
 
 // función princiapal
 const main = async () => {
-    console.log(`Hello world`);
+    
+    // Variable para almacenar la opción seleccionada por el usuario
+    let option = '';
+
+    // Ciclo iterativo para mostrar el menú, la aplicación finalizará
+    // con la tecla '0'
+    do {
+        
+        // Obtener la opción del usuario a partir de la función de mostrar
+        // el menú de opciones (ya que la función retorna una promesa es 
+        // necesario utilizar un await para esperar la acción del usuario)
+        option = await showMenu();
+
+        // Validación de la opción ingresada por el usuario. En el caso de
+        // que digite alguna opción que no se encuentre en el menú, el usuario
+        // recibirá un mensaje indicando que la opción es incorrecta.
+        if(['1', '2', '3', '4', '0'].indexOf(option) === -1) console.log(`\nThe option ${option} is wrong!`.red);
+
+        // Manejo de la acción dependiendo de la elección del usuario
+        switch (option) {
+            case '1':
+                // Read file and calculate salaries
+            break;
+            case '2':
+                // Read data and calculate salaries
+            break;
+            case '3':
+                // Show salaries calculated
+            break;
+            case '4':
+                // Show value by day and time.
+            break;
+        }
+
+        // Mientras la opción no finalice el programa, pausamos la ejecución
+        if (option !== '0') await pause();
+
+    } while (option !== '0');
+
+    // Mensaje al finalizar la ejecución del programa
+    console.log(`\nThanks!\n`.bold.magenta);
+
 }
 
 // ejecución de la función principal
---
+++ src/helpers/handlerOutput.js
@@ -0,0 +1,78 @@
+// colores para la consola
+require('colors');
+
+/**
+ * Esta función es la encargada generar el menú de opciones y presentarlo al usuario.
+ * 
+ * Se hace uso de promesas ya que se requiere de que el usuario realice alguna acción.
+ * De esta manera el menú de opciones se muestra visible hasta que el usuario realiza
+ * una acción.
+ * @returns option
+ */
+const showMenu = () => {
+
+    return new Promise(resolve => {
+        
+        // Limiar la consola cada vez que el menú se inicialice
+        console.clear();
+
+        // Cabecera del menú de opciones
+        console.log(`===========================================`.green);
+        console.log(`                Salary App                 `.yellow)
+        console.log(`===========================================\n`.green);
+
+        // Opciones del menú
+        console.log(`${'1.'.green} Read file and calculate salaries.`);
+        console.log(`${'2.'.green} Enter data and calculate salaries.`);
+        console.log(`${'3.'.green} Show calculated salaries.`);
+        console.log(`${'4.'.green} Show value by day and time.`);
+        console.log(`${'0.'.green} Exit.\n`);
+
+        // Interfaz utilizada para mostrar y recibir información del usuario
+        // por consola (no se hace uso de paquetes externos para la lectura)
+        const readLine = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout,
+        });
+
+        // Pregunta mostrada al usuario para que pueda seleccionar una de las
+        // opciones del menu
+        readLine.question(`Select a option: `, (option) => {
+            // Cierre de la interfaz de lectura | escritura
+            readLine.close();
+            // Resolución de la promesa con la opción del usuario
+            resolve(option);
+        });
+    });
+
+}
+
+/**
+ * Simular una pausa en la ejecución de la aplicación
+ * @returns nothing
+ */
+const pause = () => {
+
+    return new Promise(resolve => {
+        // Interfaz utilizada para mostrar y recibir información del usuario
+        // por consola (no se hace uso de paquetes externos para la lectura)
+        const readLine = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout,
+        });
+
+        // Mostrar información al usuario
+        readLine.question(`\nPress ${'ENTER'.green} to continue `, () => {
+            readLine.close();
+            resolve();
+        });
+    });
+}
+
+/**
+ * Exportación de funciones
+ */
+module.exports = {
+    showMenu,
+    pause
+}
